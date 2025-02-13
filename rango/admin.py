079091a55cb1926aaa3db93606f103c03e16b4e0
“Evie <2887482s@student.gla.ac.uk>
2025-02-13 20:12:31 +0000

    Customised admin page

--- rango/admin.py
+++ rango/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 admin.site.register(Category)
-admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'url')
+
+admin.site.register(Page, PageAdmin)
